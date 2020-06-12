8cacef3b3f0c3ae63acab6ed6159f7bf510aa4b8
Thomas Höllt <thoellt@me.com>
2020-06-12 19:01:15 +0200

    Adds macos 2.3.1 to website and appcast

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44,7 +44,7 @@ function init() {
     {
   		if (navigator.appVersion.indexOf("Mac")!=-1){
   				document.getElementById("download-bt").src="assets/images/download-mac.png";
-  				document.getElementById("download-link").href="download/macos/Cytosplore.2.3.0a.dmg";
+  				document.getElementById("download-link").href="download/macos/Cytosplore."+version+".dmg";
   		} else {
   				document.getElementById("download-bt").src="assets/images/download-win.png";
   				document.getElementById("download-link").href="download/win/"+version+"/install_cytosplore.exe";
